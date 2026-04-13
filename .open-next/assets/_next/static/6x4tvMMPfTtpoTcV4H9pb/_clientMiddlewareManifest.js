8fe785371ccb3fc86e228f04c4c986edcc63563c
Jonas-Michek <michek.jonas@gmail.com>
2026-04-13 21:09:35 +0200

    0.2 - Cloudflare worker

---
+++ .open-next/assets/_next/static/6x4tvMMPfTtpoTcV4H9pb/_clientMiddlewareManifest.js
@@ -0,0 +1,6 @@
+self.__MIDDLEWARE_MATCHERS = [
+  {
+    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?\\/api\\/fetch-url(\\\\.json)?[\\/#\\?]?$",
+    "originalSource": "/api/fetch-url"
+  }
+];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
