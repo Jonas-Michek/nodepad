a3d7fcf38fd62e1b73d78506c75ae229eaf282e9
Jonas-Michek <michek.jonas@gmail.com>
2026-04-13 20:36:09 +0200

    fix: clean open-next.config.ts for Cloudflare build

--- open-next.config.ts
+++ open-next.config.ts
@@ -6,21 +6,14 @@ export default {
       wrapper: "cloudflare-node",
       converter: "edge",
       proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
     },
   },
-  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
     override: {
       wrapper: "cloudflare-edge",
       converter: "edge",
       proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
     },
   },
 } satisfies OpenNextConfig;
